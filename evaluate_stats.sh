fb53d79758c8c2588061199de70ae7226095a0fc
Ekaterina Latypova <ekaterina.latypova.99@yandex.ru>
2021-05-11 13:45:25 +0300

    deleted scripts

--- evaluate_stats.sh
+++
@@ -1,9 +0,0 @@
-python3 evaluate_stats.py \
-  --model_type small \
-  --map_dir /home/ekaterina/Загрузки/project_data/map \
-  --goal_dir /home/ekaterina/Загрузки/project_data/goal_structured \
-  --heuristic_dir /home/ekaterina/Загрузки/project_data/heuristic_structured \
-  --map_to_heuristic /home/ekaterina/Загрузки/project_data/data.json \
-  --output_dir /home/ekaterina/Загрузки/project_data/results \
-  --checkpoint_path /home/ekaterina/Загрузки/project_data/results/checkpoint_6 \
-  --max_size 10
--- visualize_results.sh
+++
@@ -1,8 +0,0 @@
-python3 visualize_results.py \
-  --model_type small \
-  --map_dir /home/ekaterina/Загрузки/project_data/map \
-  --goal_dir /home/ekaterina/Загрузки/project_data/goal_structured \
-  --heuristic_dir /home/ekaterina/Загрузки/project_data/heuristic_structured \
-  --map_to_heuristic /home/ekaterina/Загрузки/project_data/data.json \
-  --output_dir /home/ekaterina/Загрузки/project_data/results \
-  --checkpoint_path /home/ekaterina/Загрузки/project_data/results/checkpoint_6
